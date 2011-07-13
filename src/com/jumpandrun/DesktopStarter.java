5ef10b5481189b4045c13ea8197512cf841c559b
Stefan Wagner <bompo@web.de>
2011-07-13 17:21:47 +0200

    switched to lwjgl

--- src/com/jumpandrun/DesktopStarter.java
+++ src/com/jumpandrun/DesktopStarter.java
@@ -2,23 +2,25 @@ package com.jumpandrun;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.backends.jogl.JoglApplication;
-import com.badlogic.gdx.backends.jogl.JoglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class DesktopStarter extends Game {
 	
 	public static void main(String[] args) {
-		JoglApplicationConfiguration config = new JoglApplicationConfiguration();
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "KPSpielundMusik";
-
-		config.fullscreen = false;
 		config.width = 800;
 		config.height = 480;
-		//fullscreen
-//		config.setFromDisplayMode(JoglApplicationConfiguration.getDesktopDisplayMode());
+//		config.setFromDisplayMode(LwjglApplicationConfiguration.getDesktopDisplayMode());
+//		config.fullscreen = true;
 		config.samples = 4;
 		config.useGL20 = true;
-		new JoglApplication(new DesktopStarter(), config);
+		config.r = 5;
+		config.g = 6;
+		config.b = 5;
+		config.a = 0;
+		new LwjglApplication(new DesktopStarter(), config);
 	}
 	
 	@Override 
